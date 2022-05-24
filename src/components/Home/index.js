6377cf5869d63fc3b14b93fc3ac65c3d898d5530
simon-tanna <gcab022109@coderacademy.edu.au>
2022-05-24 14:11:08 +1000

    completed to 27:41

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,20 @@
+import { Link } from 'react-router-dom'
+import SimonLogo from '../../assets/images/st-logo.png'
+import './index.scss';
+
 const Home = () => {
     return (
-        <div >
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br />I'm
+                <img src={SimonLogo} alt="developer" />
+                Simon Tanna
+                <br />
+                web developer
+                </h1>
+                <h2>Full-stack Developer</h2>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+            </div>
 
         </div>
     )
