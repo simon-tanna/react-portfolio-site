36dd757e9b51e74738bf749c4f8d5b4ade9c2263
simon-tanna <gcab022109@coderacademy.edu.au>
2022-05-25 07:44:24 +1000

    added animate text. 36:09

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['S', 'i', 'm', 'o', 'n', ' ', 'T', 'a', 'n', 'n', 'a']
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>Hi, 
+                
                 <br />
-                I'm Simon Tanna
+                I'm <AnimatedLetters letterClass={letterClass}
+                stringArray={nameArray}
+                index={15}
+                />
                 <br />
                 web developer
                 </h1>
