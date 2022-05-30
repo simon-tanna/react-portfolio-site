3461551a5e1babef0fbeeba731c8ade1690eee07
simon-tanna <gcab022109@coderacademy.edu.au>
2022-05-30 11:27:11 +1000

    fixed animation hover

--- src/components/AnimatedLetters/index.js
+++ src/components/AnimatedLetters/index.js
@@ -1,17 +1,15 @@
-import './index.scss';
+import './index.scss'
 
 const AnimatedLetters = ({ letterClass, stringArray, index }) => {
-    return (
-        <span>
-            {
-                stringArray.map((char, i) => (
-                    <span key={char + i} className={`${letterClass} _${i + index }`}>
-                        {char}
-                    </span>
-                ))
-            }
+  return (
+    <span>
+      {stringArray.map((char, i) => (
+        <span key={char + i} className={`${letterClass} _${i + index}`}>
+          {char}
         </span>
-    )
+      ))}
+    </span>
+  )
 }
 
 export default AnimatedLetters
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,38 +1,71 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import './index.scss';
+import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['S', 'i', 'm', 'o', 'n', ' ', 'T', 'a', 'n', 'n', 'a']
-    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['S', 'i', 'm', 'o', 'n', ' ', 'T', 'a', 'n', 'n', 'a']
+  const jobArray = [
+    'f',
+    'u',
+    'l',
+    'l',
+    '-',
+    's',
+    't',
+    'a',
+    'c',
+    'k',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
 
-    return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                <span className="letterClass">H</span>
-                <span className={`${letterClass} _12`}>i,</span>
-                <br />
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m </span>
-                <AnimatedLetters letterClass={letterClass}
-                stringArray={nameArray}
-                index={15}
-                />
-                <br />
-                <AnimatedLetters letterClass={letterClass}
-                stringArray={jobArray}
-                index={22}
-                />
-                </h1>
-                <h2>Full-stack Developer</h2>
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
-            </div>
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
 
-        </div>
-    )
+  return (
+    <>
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m&nbsp;</span>
+          <AnimatedLetters
+            letterClass={letterClass}
+            stringArray={nameArray}
+            index={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            stringArray={jobArray}
+            index={26}
+          />
+        </h1>
+        <h2>Junior Developer</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+    </>
+  )
 }
 
 export default Home
